6a038899b97ccaea8c92ccba2bab82b6b3b401ef
Evgeny Makhinya <eugene.makhinya@gmail.com>
2020-07-14 19:36:18 +0100

    some step to open a flask

--- cont_a/cont_a.py
+++ cont_a/cont_a.py
@@ -1,36 +1,45 @@
 import os
 import requests
-import cryptography
 import json
 from json2xml import json2xml
 from json2xml.utils import readfromstring
+from cryptography.fernet import Fernet
+import flask
 
-KEY = os.getenv('KEY_ENCRYPT')
-
-# reading the content of file
-try:
+if __name__ == "__main__":
+    
+    key = os.getenv('KEY_ENCRYPT')
+    key = Fernet.generate_key()
+    print('KEY = ',key)
+    #try:
+    # reading the content of file
     json_files = open('json_list.txt', 'r')
     i = 1 # count number of xml files
     for file_json in json_files.readlines():
         if not file_json.startswith("#"):
-            try:
-                #path = os.path.join(os.path.dirname(os.path.realpath('__file__')), file_json)
-                opened_json = open(file_json.strip())
-                json_out = str(json.load(opened_json)).replace('\'', '\"')
-                data = readfromstring(json_out)
-                xml_out = json2xml.Json2xml(data, wrapper="all", pretty=True, attr_type=False).to_xml()
-                xml_file = open('file' + str(i) + '.xml', 'w')
-                xml_file.write(xml_out)
-                i = i + 1
-            except:
-                print("File - " + file_json + " cannot be opened")
-                exit()
-            finally:
-                xml_file.close()
-                opened_json.close()
+        #try:
+            #path = os.path.join(os.path.dirname(os.path.realpath('__file__')), file_json)
+            opened_json = open(file_json.strip())
+            json_out = str(json.load(opened_json)).replace('\'', '\"')
+            data = readfromstring(json_out)
+            xml_out = json2xml.Json2xml(data, wrapper="all", pretty=True, attr_type=False).to_xml()
+            xml_out_encode = xml_out.encode()
+            fernet = Fernet(key)
+            xml_encrypted = fernet.encrypt(xml_out_encode)
+            print(xml_encrypted)
+            #xml_file = open('file' + str(i) + '.xml', 'w')
+            #xml_file.write(str(xml_encrypted))
+            #i = i + 1
+        #except:
+            #print("File - " + file_json + " cannot be opened")
+        #    exit()
+        #finally:
+            #xml_file.close()
+            opened_json.close()
 
-except:
-    print("The list or JSON file cannot be opened or something goes worng, please check the file format/permissions")
-    exit()
-finally:
+    #except:
+    #print("The list or JSON file cannot be opened or something goes worng, please check the file format/permissions")
+    #    exit()
+    #finally:
     json_files.close()
+
